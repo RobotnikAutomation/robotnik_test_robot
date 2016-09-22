ddde7abbb1505909fce86c3938d1067d7efcf7ad
carlos3dx <carlosbetera3dx@gmail.com>
2016-09-22 09:50:04 +0200

    Addec configuration files for move_base and the possibility to test omnidirectional robots

--- robotnik_test_robot_test_square/src/test_square.cpp
+++ robotnik_test_robot_test_square/src/test_square.cpp
@@ -21,15 +21,19 @@ int main(int argc, char** argv){
   ros::init(argc, argv, "test_square");
   int i, numLaps, watchdog_secs;
   std::string target_frame_id;
-  float side_size, max_vel_x, max_vel_theta, acc_lim_x, acc_lim_theta;
+  float side_size, max_vel_x, max_vel_y, max_vel_theta, acc_lim_x, acc_lim_y, acc_lim_theta;
+  bool omni_movement;
   ros::NodeHandle nh("~");
   
   nh.param<int>("numLaps", numLaps, 2);
   nh.param<int>("watchdog_secs", watchdog_secs, 20);
   nh.param<float>("side_size", side_size, 2.0);
+  nh.param<bool>("omni_movement", omni_movement, false);
   nh.param<float>("max_vel_x", max_vel_x, 2.0);
+  nh.param<float>("max_vel_y", max_vel_y, 2.0);
   nh.param<float>("max_vel_theta", max_vel_theta, 1.0);
   nh.param<float>("acc_lim_x", acc_lim_x, 1.0);
+  nh.param<float>("acc_lim_y", acc_lim_y, 1.0);
   nh.param<float>("acc_lim_theta", acc_lim_theta, 1.0);
   nh.param<std::string>("target_frame_id", target_frame_id, "odom");
 
@@ -59,6 +63,21 @@ int main(int argc, char** argv){
   ss << acc_lim_theta;
   aux = ss.str();
   system(("rosrun dynamic_reconfigure dynparam set /move_base/TebLocalPlannerROS acc_lim_theta "+aux).c_str());
+  
+  if(omni_movement){
+	  ss.str("");
+	  ss.clear();
+	  ss << max_vel_y;
+	  aux = ss.str();
+	  system(("rosrun dynamic_reconfigure dynparam set /move_base/TebLocalPlannerROS max_vel_y "+aux).c_str());
+	  ss.str("");
+	  ss.clear();
+	  ss << acc_lim_y;
+	  aux = ss.str();
+	  system(("rosrun dynamic_reconfigure dynparam set /move_base/TebLocalPlannerROS acc_lim_y "+aux).c_str());
+	  system("rosrun dynamic_reconfigure dynparam set /move_base/TebLocalPlannerROS weight_kinematics_nh 1");
+		 
+  }
   
   move_base_msgs::MoveBaseGoal goal;
 
@@ -96,8 +115,10 @@ int main(int argc, char** argv){
 
 	  goal.target_pose.pose.position.x = side_size;
 	  goal.target_pose.pose.position.y = side_size/2;
-	  goal.target_pose.pose.orientation.z = 0.71;
-	  goal.target_pose.pose.orientation.w = 0.71;
+	  if(!omni_movement){
+		  goal.target_pose.pose.orientation.z = 0.71;
+		  goal.target_pose.pose.orientation.w = 0.71;
+	  }
 
 	  ROS_INFO("Sending goal");
 	  ac.sendGoal(goal);
@@ -109,8 +130,10 @@ int main(int argc, char** argv){
 
 	  goal.target_pose.pose.position.x = side_size;
 	  goal.target_pose.pose.position.y = side_size;
-	  goal.target_pose.pose.orientation.z = 1;
-	  goal.target_pose.pose.orientation.w = 0.0;
+	  if(!omni_movement){
+		  goal.target_pose.pose.orientation.z = 1;
+		  goal.target_pose.pose.orientation.w = 0.0;
+	  }
 
 	  ROS_INFO("Sending goal");
 	  ac.sendGoal(goal);
@@ -148,8 +171,10 @@ int main(int argc, char** argv){
 
 	  goal.target_pose.pose.position.x = 0.0;
 	  goal.target_pose.pose.position.y = side_size/2;
-	  goal.target_pose.pose.orientation.z = 0.71;
-	  goal.target_pose.pose.orientation.w = -0.71;
+	  if(!omni_movement){
+		  goal.target_pose.pose.orientation.z = 0.71;
+		  goal.target_pose.pose.orientation.w = -0.71;
+	  }
 
 	  ROS_INFO("Sending goal");
 	  ac.sendGoal(goal);
